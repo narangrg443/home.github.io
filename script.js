871519c58e80028dbf73c9c5d8009703e6cfce87
narangrg443 <narangrg48@gmail.com>
2024-02-21 08:12:56 +0545

    initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+const menuList=document.querySelector('.menu-list');
+const iconMenu=document.querySelector(".icon-menu");
+const iconCross=document.querySelector(".icon-cross");
+const iconSearch=document.querySelector('.icon-search');
+const searchBar=document.querySelector('.input-search')
+
+
+iconMenu.addEventListener('click',()=>{
+       toggle();
+   menuList.classList.toggle('show-menulist');
+    
+})
+
+iconCross.addEventListener('click',()=>{
+      toggle();
+    menuList.classList.toggle('show-menulist');
+})
+
+
+iconSearch.addEventListener('click',()=>{
+  
+searchBar.classList.toggle('show-search');
+
+
+})
+
+
+function toggle(){
+    iconMenu.classList.toggle('hide');
+    iconCross.classList.toggle('hide');
+}
+
+
