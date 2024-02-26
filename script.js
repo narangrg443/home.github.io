6c227d56bf8eccb66c33b8e232ba7346b8bc4058
narangrg443 <narangrg48@gmail.com>
2024-02-26 21:42:24 +0545

    added slide image background

--- script.js
+++ script.js
@@ -32,3 +32,20 @@ function toggle(){
 
 
 document.getElementById("currentYear").textContent = new Date().getFullYear();
+
+
+
+//image slider
+const sliderImage=document.querySelectorAll('.slide');
+let index=0;
+function changeBackground(){
+    sliderImage[index].classList.remove("show-background");
+    
+    index++;
+    if(index>=sliderImage.length){
+        index=0;
+    }
+    sliderImage[index].classList.add("show-background");
+}
+sliderImage[index].classList.add("show-background");
+setInterval(changeBackground,3000);
